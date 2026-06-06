e7a2597ba90715e887d4b2df8e9299c1fdd16be4
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-06 22:10:25 +0100

    feat: add NotBlank annotations for Login request body

--- src/main/java/com/interiordesignplanner/authentication/UserLoginDTO.java
+++ src/main/java/com/interiordesignplanner/authentication/UserLoginDTO.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "Request body for user login")
 @Data
@@ -12,9 +13,11 @@ import lombok.NoArgsConstructor;
 public class UserLoginDTO {
 
     @Schema(description = "User username", example = "sophiethompson")
+    @NotBlank(message = "Username is required")
     private String username;
 
     @Schema(description = "User password", example = "xds6fs46yG7x9s")
+    @NotBlank(message = "Password is required")
     private String password;
 
 }
