13dae7d8ca38e881278375a950a5b998c3e5f22a
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-06-09 22:26:45 +0100

    feat: add DesignerService and DesignerController

--- src/main/java/com/interiordesignplanner/designer/DesignerService.java
+++ src/main/java/com/interiordesignplanner/designer/DesignerService.java
@@ -1,15 +1,76 @@
 package com.interiordesignplanner.designer;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.interiordesignplanner.authentication.User;
+import com.interiordesignplanner.authentication.UserRepository;
 import com.interiordesignplanner.exceptions.UserNotFoundException;
+import com.interiordesignplanner.mapper.DesignerMapper;
 
 @Service
 public class DesignerService {
+    private final UserRepository userRepository;
+    private final DesignerMapper designerMapper;
     private final DesignerRepository designerRepository;
 
-    public DesignerService(DesignerRepository designerRepository) {
+    public DesignerService(DesignerRepository designerRepository, DesignerMapper designerMapper,
+            UserRepository userRepository) {
         this.designerRepository = designerRepository;
+        this.designerMapper = designerMapper;
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Return the Designer's profile
+     * 
+     * Reduces code repetition
+     * 
+     * @param username retrieves the current auth user
+     * @throws UserNotFoundException if the user is not found
+     * @return the designer's profile
+     */
+    @PreAuthorize("hasRole('DESIGNER')")
+    public DesignerProfileDTO getProfile(String username) {
+        User authUser = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User is not found"));
+
+        Designer authDesigner = findDesigner(authUser.getId());
+
+        return designerMapper.toDto(authDesigner);
+    }
+
+    /**
+     * Updates auth designer details on the system.
+     * 
+     * <p>
+     * Use this method to modify details of a authenticated designer on
+     * the system.
+     * </p>
+     * 
+     * @param DesignerProfileUpdateDTO the designer object is updated
+     * @param username                 retrieves the current auth user
+     * @throws UserNotFoundException if the designer is not found
+     * @return the updated designer object
+     */
+    @PreAuthorize("hasRole('DESIGNER')")
+    @Transactional
+    public DesignerProfileDTO updateProfile(DesignerProfileUpdateDTO designerProfileUpdateDTO,
+            String username) {
+
+        User authUser = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User is not found"));
+
+        Designer authDesigner = findDesigner(authUser.getId());
+
+        designerMapper.updateProfile(designerProfileUpdateDTO, authDesigner);
+
+        userRepository.save(authUser);
+        designerRepository.save(authDesigner);
+
+        return designerMapper.toDto(authDesigner);
     }
 
     /**
@@ -17,7 +78,7 @@ public class DesignerService {
      * 
      * Reduces code repetition
      * 
-     * @param id retrieves the user object to be deleted
+     * @param id retrieves the user object
      * @throws UserNotFoundException if the user is not found
      * @return the user
      */
