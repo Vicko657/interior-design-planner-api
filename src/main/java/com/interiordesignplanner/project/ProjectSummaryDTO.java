9f26ccf705b9d62396d5f0fd85e01f1ecea9ecd6
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-01 16:31:00 +0100

    refactor: add clientId, remainingBudget and room fields in ProjectSummaryDTO

--- src/main/java/com/interiordesignplanner/project/ProjectSummaryDTO.java
+++ src/main/java/com/interiordesignplanner/project/ProjectSummaryDTO.java
@@ -3,6 +3,8 @@ package com.interiordesignplanner.project;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.interiordesignplanner.room.RoomType;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -19,6 +21,9 @@ public class ProjectSummaryDTO {
     @Schema(description = "Project id", example = "1")
     private Long id;
 
+    @Schema(description = "Client id", example = "2")
+    private Long clientId;
+
     @Schema(description = "Client full name", example = "Tom Jackson")
     private String clientName;
 
@@ -32,6 +37,9 @@ public class ProjectSummaryDTO {
     @Schema(description = "Project budget", example = "5000.00")
     private BigDecimal budget;
 
+    @Schema(description = "Project remianing budget", example = "3500.00")
+    private BigDecimal remainingBudget;
+
     @Schema(description = "Project start date", example = "2026-01-08")
     private LocalDate startDate;
 
@@ -40,5 +48,24 @@ public class ProjectSummaryDTO {
 
     @Schema(description = "Project description", example = "Full redesign of living room with contemporary furniture and lighting")
     private String description;
+
+    @Schema(description = "Room id", example = "50")
+    private Long roomId;
+
+    @Schema(description = "Room type", example = "LIVING_ROOM")
+    @Enumerated(EnumType.STRING)
+    private RoomType room;
+
+    @Schema(description = "Room length", example = "6.4")
+    private Double length;
+
+    @Schema(description = "Room height", example = "5.0")
+    private Double height;
+
+    @Schema(description = "Room width", example = "3.5")
+    private Double width;
+
+    @Schema(description = "Unit of dimensions", example = "M")
+    private String unit;
 
 }
