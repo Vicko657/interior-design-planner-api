f44afa4dbee8ef6e3c83d8c3d6588c54a0aab528
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-01 21:17:21 +0100

    fix: update query join for findProjectsByDesignerId

--- src/main/java/com/interiordesignplanner/project/ProjectRepository.java
+++ src/main/java/com/interiordesignplanner/project/ProjectRepository.java
@@ -58,7 +58,7 @@ public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpec
      * @param pageable pagination info
      * @return paginated list of clients
      */
-    @Query("SELECT new com.interiordesignplanner.project.ProjectDTO(p.id, CONCAT(c.firstName,' ', c.lastName), p.projectName, p.status, p.budget, p.startDate, p.dueDate, p.meetingURL, p.description, p.completedAt, r.type ) FROM Project p JOIN Room r LEFT JOIN p.client c LEFT JOIN c.designer d LEFT JOIN d.user u WHERE c.designer.id = :userId GROUP BY p.id")
+    @Query("SELECT new com.interiordesignplanner.project.ProjectDTO(p.id, CONCAT(c.firstName,' ', c.lastName), p.projectName, p.status, p.budget, p.startDate, p.dueDate, p.meetingURL, p.description, p.completedAt, r.type ) FROM Project p LEFT JOIN p.room r LEFT JOIN p.client c LEFT JOIN c.designer d LEFT JOIN d.user u WHERE c.designer.id = :userId GROUP BY p.id")
     Page<ProjectDTO> findProjectsByDesignerId(@Param("userId") Long userId, Pageable pageable);
 
     @Query("SELECT COUNT(DISTINCT p.id) FROM Project p LEFT JOIN p.client c LEFT JOIN c.designer d LEFT JOIN d.user u WHERE c.designer.id = :userId AND p.status = :status GROUP BY p.id")
