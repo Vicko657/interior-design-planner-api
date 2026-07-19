1e2039c15744852739cc0f0e17959527a92e3048
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-19 17:51:37 +0100

    feat: add TaskDTO

---
+++ src/main/java/com/interiordesignplanner/task/TaskDTO.java
@@ -0,0 +1,25 @@
+package com.interiordesignplanner.task;
+
+import java.time.LocalDate;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
+
+public class TaskDTO {
+
+    @Schema(description = "Task name", example = "Order lighting and furniture")
+    private String taskName;
+
+    @Schema(description = "Task description", example = "Check the inventory for the items")
+    private String task;
+
+    @Schema(description = "Task description", example = "2026-03-02")
+    private LocalDate date;
+
+    @Schema(description = "Task completed?", example = "true")
+    private boolean completed;
+
+    @Schema(description = "Project name", example = "Luxury Master Bedroom")
+    private String projectName;
+
+}
