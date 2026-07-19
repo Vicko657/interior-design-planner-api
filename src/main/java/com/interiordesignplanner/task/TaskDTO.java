1ae736919d3cbd5d03cc5624b6afb510fd13d4ee
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-19 18:34:05 +0100

    fix: add constructor to TaskDTO

--- src/main/java/com/interiordesignplanner/task/TaskDTO.java
+++ src/main/java/com/interiordesignplanner/task/TaskDTO.java
@@ -4,7 +4,14 @@ import java.time.LocalDate;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Schema(description = "Response body for a task")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskDTO {
 
     @Schema(description = "Task name", example = "Order lighting and furniture")
