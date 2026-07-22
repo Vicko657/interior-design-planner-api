4972bb8d86a4efb8d69eed221eac4903d677ff7e
Victoria Olusegun <victoriaolusegun@gmail.com>
2026-07-22 15:23:50 +0100

    fix: update misspelled enum

--- src/main/java/com/interiordesignplanner/room/RoomType.java
+++ src/main/java/com/interiordesignplanner/room/RoomType.java
@@ -2,9 +2,9 @@ package com.interiordesignplanner.room;
 
 /** Enum for different types of room types */
 public enum RoomType {
-    
+
     HALLWAY, BATHROOM, LIVING_ROOM, KITCHEN,
     DINING_ROOM, UTILITY_ROOM, BEDROOM,
-    CONSERVETORY, BASEMENT, LOFT;
+    CONSERVATORY, BASEMENT, LOFT;
 
 }
